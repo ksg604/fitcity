0c2ee8946a847aa52c862a1d21a8692226a840e8
Kevin San Gabriel <ksangabr@sfu.ca>
2023-01-26 06:34:42 -0800

    fix: styling fixes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,32 +7,34 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return(
     <div className={styles["Home"]}>
-      <main>
-        <h2>Products</h2>
-        <Link to="/products/footwear">
-          <div className={styles["product-link-container"]}>
-            <img className={styles["product-img"]} src={Footwear} alt="Footwear"/>
-            <div className={styles["product-img-overlay"]}>
-              <p className={styles["product-overlay-text"]}>Lifting Shoes</p>
+      <main className={styles["main"]}>
+        <h2>Collections</h2>
+        <section className={styles["collections"]}>
+          <Link to="/products/footwear" className={styles["product-link"]}>
+            <div className={styles["product-link-container"]}>
+              <img className={styles["product-img"]} src={Footwear} alt="Footwear"/>
+              <div className={styles["product-img-overlay"]}>
+                <p className={styles["product-overlay-text"]}>Lifting Shoes</p>
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link to="/products/lifting-belts">
-          <div className={styles["product-link-container"]}>
-            <img className={styles["product-img"]} src={Belt} alt="Belt"/>
-            <div className={styles["product-img-overlay"]}>
-              <p className={styles["product-overlay-text"]}>Lifting Belts</p>
+          </Link>
+          <Link to="/products/lifting-belts" className={styles["product-link"]}>
+            <div className={styles["product-link-container"]}>
+              <img className={styles["product-img"]} src={Belt} alt="Belt"/>
+              <div className={styles["product-img-overlay"]}>
+                <p className={styles["product-overlay-text"]}>Lifting Belts</p>
+              </div>
             </div>
-          </div>
-        </Link>
-        <Link to="products/knee-sleeves">
-          <div className={styles["product-link-container"]}>
-            <img className={styles["product-img"]} src={KneeSleeve} alt="Knee Sleeve"/>
-            <div className={styles["product-img-overlay"]}>
-              <p className={styles["product-overlay-text"]}>Knee Sleeves</p>
+          </Link>
+          <Link to="products/knee-sleeves" className={styles["product-link"]}>
+            <div className={styles["product-link-container"]}>
+              <img className={styles["product-img"]} src={KneeSleeve} alt="Knee Sleeve"/>
+              <div className={styles["product-img-overlay"]}>
+                <p className={styles["product-overlay-text"]}>Knee Sleeves</p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </section>
       </main>
     </div>
   )
