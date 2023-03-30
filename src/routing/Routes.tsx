209e5bd0419c50076c56324fc2879d4036e4f758
Kevin San Gabriel <ksangabr@sfu.ca>
2023-03-30 03:41:47 -0700

    fix: refresh spa screen issue

--- src/routing/Routes.tsx
+++ src/routing/Routes.tsx
@@ -1,4 +1,4 @@
-import { Routes as AppRoutes, Route } from "react-router-dom";
+import { Routes as AppRoutes, Route, useNavigate } from "react-router-dom";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
 import MyProfile from '../screens/MyProfile';
@@ -9,11 +9,18 @@ import Products from "../screens/Products";
 import ResetPassword from "../screens/ResetPassword";
 import ForgotPassword from "../screens/ForgotPassword";
 import Cart from "../screens/Cart";
+import { useEffect } from "react";
 
 export default function Routes() {
 
   const isLoggedIn = useAppSelector(state => state.auth.loggedIn);
   const token = useAppSelector(state => state.auth.accessToken);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = /#!(\/.*)$/.exec(window.location.href);
+    if (path) navigate(path[1], { replace: true });
+  }, []);
 
   return(
     <AppRoutes>
